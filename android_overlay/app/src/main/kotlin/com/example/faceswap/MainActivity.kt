8db6b15387b179c48856d5593e1c9f9e512ea08c
76haoyu <1018838033@qq.com>
2026-08-27 17:45:47 +0800

    Update MainActivity.kt

--- android_overlay/app/src/main/kotlin/com/example/faceswap/MainActivity.kt
+++ android_overlay/app/src/main/kotlin/com/example/faceswap/MainActivity.kt
@@ -6,7 +6,6 @@ import io.flutter.embedding.engine.FlutterEngine
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        // 注册真实端侧换脸原生模块（演示模式下 Dart 不会调用，故不影响默认运行）
-        MlFaceSwapPlugin.registerWith(flutterEngine)
+        MlFaceSwapPlugin.registerWith(flutterEngine, this)
     }
 }
